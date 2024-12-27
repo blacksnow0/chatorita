069e93575ac659606200eeae1f6c7eddb688f922
blacksnow0 <86392451+blacksnow0@users.noreply.github.com>
2024-12-27 17:58:11 +0530

    updated to mongo atlas

--- server/server.js
+++ server/server.js
@@ -6,12 +6,14 @@ const cors = require("cors");
 
 connectDb();
 
+require("dotenv").config();
+
 app.use(express.json());
 
 app.use(cors());
 
-app.listen("5003", () => {
-  console.log("Server is running on port 5003");
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
 
 app.get("/", (req, res) => {
